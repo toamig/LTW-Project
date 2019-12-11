aa85811f8a122a17dc580bbdbfcafd620d34c541
unknown <carlosmiguelvieira@hotmail.com>
2019-12-11 07:51:55 +0000

    raw house.php

---
+++ pages/house.php
@@ -0,0 +1,50 @@
+<?php 
+    include_once('../templates/templates.php');
+    include_once('../database/connection.php');
+    include_once('../database/db_utils.php');
+    drawHeader(); 
+
+    if(isset($_GET['id'])){
+
+        $id = $_GET['id'];
+
+        $house = getHouse($id);
+?>
+
+        <nav class="house-body">
+
+            <h2><?=$house["title"]?></h2>
+
+            <p><?=$house["address"].", ".$house["state"].", ".$house["postCode"]?></p>
+
+            <img src="../images/<?=$house['image']?>" alt="HouseImg">
+
+            <div id="house-info">
+
+                <p>Price: <?=$house["price"]?>$</p>
+
+                    <div id="house-info">
+
+                        <h3>Description</h3>
+
+                        <p><?=$house["description"]?></p>
+
+                    </div>
+
+                <p>advertiser: <a><?=$house["username"]?></a></p>
+
+            </div>
+
+        </nav>
+
+
+<?php 
+
+    }else{ 
+
+        header('Location: home.php');
+
+    }
+
+    drawFooter(); 
+?>
