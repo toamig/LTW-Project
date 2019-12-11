cfed4cc6ed0835ffc7aba6a88bbe5177593d8e0a
Paulo Rocha <paulojunior_29@hotmail.com>
2019-12-11 15:16:17 +0000

    mergin

--- pages/rental.php
+++ pages/rental.php
@@ -1,24 +1,19 @@
 <?php 
     include_once('../templates/templates.php');
+    include_once('../database/session.php');
+    include_once('../database/connection.php');
+    include_once('../database/db_utils.php');
     drawHeader(); 
-?>
 
-        <nav id="advertisement">
-                <form action="add.php" method="post" id="advertisement">
-                    <input type="text" name="title" id="title" required="true" placeholder="Announcement title"><br>
-                    <!-- <span>Phone Number:</span><br> -->
-                    <input type="number" name="price" id="price" required="true" placeholder="Price"><br>
-                    <!-- <span>Confirm Password:</span><br> -->
-                    <input type="text" name="address1" required="true" placeholder="Address" id="address"><br>
-                    <input type="number" name="addrress2" required="true" placeholder="Number"><br>
-                    <input type="text" placeholder="City" required="true" name="City"><br>
-                    <input type="text" placeholder="Country"  required="true"name="Country"><br>
-                    <textarea rows="5" cols="50" required="true" placeholder="Description"></textarea>
-                    <br>
-                    <a href="?">Add images from your place.</a>
-                </form>
-                
-        </nav>
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $house = getHouse($id);
+    }
+?>
+    <div id="rental">
+        <h2><?=$house["title"]?></h2>
+        <img src="<?=$house['image']?>">
+    </div>
 
 <?php 
     drawFooter(); 
